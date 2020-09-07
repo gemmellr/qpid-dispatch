dbf57f6ee76c1d20599a5e69f95ba7d4a3c4839a
Robbie Gemmell <robbie@apache.org>
2020-09-07 16:46:48 +0100

    DISPATCH-1762: clarify updated error message, verify failure logged by connecting route, remove extraneous client certificate configuration

--- tests/system_tests_ssl.py
+++ tests/system_tests_ssl.py
@@ -876,11 +876,8 @@ class RouterTestSslInterRouterWithInvalidPathToCA(RouterTestSslBase):
             # setup chain of calls to pn_ssl_domain_* functions.
             ('sslProfile', {'name': 'ssl-profile-tls-all',
                             'caCertFile': cls.ssl_file('ca-certificate-INVALID-FILENAME.pem'),
-                            'certFile': cls.ssl_file('client-certificate.pem'),
-                            'privateKeyFile': cls.ssl_file('client-private-key.pem'),
                             'ciphers': 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:' \
-                                       'DH+AES:RSA+AESGCM:RSA+AES:!aNULL:!MD5:!DSS',
-                            'password': 'client-password'})
+                                       'DH+AES:RSA+AESGCM:RSA+AES:!aNULL:!MD5:!DSS'})
         ])
 
         cls.routers.append(cls.tester.qdrouterd("A", config_a, wait=False))
@@ -935,6 +932,22 @@ class RouterTestSslInterRouterWithInvalidPathToCA(RouterTestSslBase):
                     break;
             time.sleep(sleep_time)
         self.assertTrue(verified, "Log line containing '%s' not seen for both connectors in QDR.B log" % pattern)
+
+        verified = False
+        pattern1 = "Connection to %s failed:" % host_port_1
+        pattern2 = "Connection to %s failed:" % host_port_2
+        for tries in range(int(poll_duration / sleep_time)):
+            logfile = os.path.join(self.routers[1].outdir, self.routers[1].logfile)
+            if os.path.exists(logfile):
+                with  open(logfile, 'r') as router_log:
+                    log_lines = router_log.read().split("\n")
+                e1_lines = [s for s in log_lines if pattern1 in s]
+                e2_lines = [s for s in log_lines if pattern2 in s]
+                verified = len(e1_lines) > 0 and len(e2_lines) > 0
+                if verified:
+                    break;
+            time.sleep(sleep_time)
+        self.assertTrue(verified, "Log line containing '%s' or '%s' not seen in QDR.B log" % (pattern1, pattern2))
 
         # Show that router A does not have router B in its network
         router_nodes = self.get_router_nodes()
